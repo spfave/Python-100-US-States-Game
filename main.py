bc73561219fd3bfd514d6da3f73ca02896780d34
sfave <sebpfave@gmail.com>
2020-12-01 20:17:25 -0500

    Starting Files

---
+++ main.py
@@ -0,0 +1,12 @@
+import turtle
+
+
+# Main
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "./us-states-game-start/blank_states_img.gif"
+# screen.addshape(image)
+
+screen.bgpic(image)
+
+screen.exitonclick()
