6b9d07358917f2609d3f15ed3aa9094597fd2884
sfave <sebpfave@gmail.com>
2020-12-01 20:28:19 -0500

    Retrieve map coordinates

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import turtle
 
 
+# Functions
+def get_mouse_click_coord(x, y):
+    print(x, y)
+
+
 # Main
+# Setup
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "./us-states-game-start/blank_states_img.gif"
 screen.bgpic(image)
 
-screen.exitonclick()
+screen.onscreenclick(fun=get_mouse_click_coord)
+
+# Game
+screen.mainloop()  # use screen.mainloop() or turtle.done()
