7b9be0850d177e1aa46b781dcafc1bd7730b0b18
sfave <sebpfave@gmail.com>
2020-12-01 21:06:30 -0500

    Write state name if guessed correct on map

--- main.py
+++ main.py
@@ -11,14 +11,22 @@ screen.title("U.S. States Game")
 image = "./us-states-game-start/blank_states_img.gif"
 screen.bgpic(image)
 
-state_data = pd.read_csv("./us-states-game-start/50_states.csv")
-print(state_data.state.values)
+turtle = turtle.Turtle()
+turtle.hideturtle()
+turtle.penup()
+
+us_state_data = pd.read_csv("./us-states-game-start/50_states.csv")
+
 
 answer_state = screen.textinput(
     title="Name a State", prompt="Enter a U.S. State name").title()
 
-if answer_state in state_data.state.values:
+if answer_state in us_state_data.state.values:
     print("correct")
+    state_data = us_state_data[us_state_data.state == answer_state]
+    turtle.goto(int(state_data.x), int(state_data.y))
+    turtle.write(answer_state)
+
 
 # Game
 screen.mainloop()  # use screen.mainloop() or turtle.done()
