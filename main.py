b36e4fea65b049eb0345a616ea10a15bc72c1b47
sfave <sebpfave@gmail.com>
2020-12-01 20:21:07 -0500

    Get background image running

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import turtle
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "./us-states-game-start/blank_states_img.gif"
-# screen.addshape(image)
-
 screen.bgpic(image)
 
 screen.exitonclick()
