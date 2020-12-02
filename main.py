21f5caf3b3ee362f4a03d1809021d56d3f155b4f
sfave <sebpfave@gmail.com>
2020-12-01 20:49:45 -0500

    Check correct state entry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import turtle
-
+import pandas as pd
 
 # Functions
 
@@ -11,9 +11,14 @@ screen.title("U.S. States Game")
 image = "./us-states-game-start/blank_states_img.gif"
 screen.bgpic(image)
 
+state_data = pd.read_csv("./us-states-game-start/50_states.csv")
+print(state_data.state.values)
 
 answer_state = screen.textinput(
     title="Name a State", prompt="Enter a U.S. State name").title()
+
+if answer_state in state_data.state.values:
+    print("correct")
 
 # Game
 screen.mainloop()  # use screen.mainloop() or turtle.done()
